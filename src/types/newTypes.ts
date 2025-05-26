8be86408aa8f9ceb2fba9f325e7729fae58be404
kanteretePC <kacperelsky2@gmail.com>
2025-05-26 18:49:18 +0200

    comments

--- src/types/newTypes.ts
+++ src/types/newTypes.ts
@@ -6,17 +6,20 @@ export type Exercise = {
   weight: number;
 };
 
+//signle day - Push day, Leg day
 export type Training = {
   name: string;
   exercises: Exercise[];
 };
 
+//Training plan (fbw, 4-day split, 5-day split)
 export type WorkoutPlan = {
   id: string;
   name: string;
   trainings: Training[];
 };
 
+//Schedule of week
 export type WeeklyWorkoutSchedule = {
   planId: string;
   days: {
@@ -24,6 +27,7 @@ export type WeeklyWorkoutSchedule = {
   };
 };
 
+//Date assignment to training
 export type DailyTrainingAssignment = {
   date: string;
   planId: string;
